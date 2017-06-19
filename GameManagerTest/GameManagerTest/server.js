d7770731c75add9c449cd44b58203becf2e62d97
schandwani (⌐■_■) <schandwani@optimalblue.com>
2017-06-19 09:16:29 -0500

    Init comit

---
+++ GameManagerTest/GameManagerTest/server.js
@@ -0,0 +1,5 @@
+﻿//Key: 5261671DC0E2BBF859874237B0E0C56F
+//Domain Name: www.GameManagerShawn.com
+//https://codepen.io/johnchristopherjones/post/how-do-i-use-the-steam-api-in-my-web-app
+
+var express = require('express');
